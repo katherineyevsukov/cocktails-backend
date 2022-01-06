e7993edf6897c2e81b367025a137e7efbaf67ec3
Kat <kyevsukov@gmail.com>
2022-01-06 15:23:25 -0500

    created users table

--- api/data/migrations/20220106195535_first-migration.js
+++ api/data/migrations/20220106195535_first-migration.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
+exports.up = async (knex) => {
+    await knex.schema
+      .createTable('users', (users) => {
+        users.increments('id')
+        users.string('email', 254).notNullable().unique()
+        users.string('password', 50).notNullable()
+        users.string('first_name', 120).notNullable()
+        users.string('last_name', 120).notNullable()
+        users.string('phone', 25).notNullable()
+        users.timestamp('deleted_at')
+        users.timestamps(false, true)
+      })
+  }
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists('users')
+  }
