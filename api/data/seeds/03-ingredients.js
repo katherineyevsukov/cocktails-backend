07b46689980bae107b7896bec0be18089c307165
Kat <kyevsukov@gmail.com>
2022-01-27 17:14:05 -0500

    added ingredient seeds

--- api/data/seeds/03-ingredients.js
+++ api/data/seeds/03-ingredients.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
       { name: 'rosemary simple syrup', alcoholic: false, category: "other"},
       { name: 'Carpano Antica Sweet Vermouth', alcoholic: true, category: "cordial"},
       { name: 'Michter\'s US1 Rye Whiskey', alcoholic: true, category: "bourbon/whiskey"},
-      { name: 'Angostura Aromatic Bitters', alcoholic: true, category: ""},
-      { name: 'Cointreau Orange Liquer', alcoholic: true, category: "" },
-      { name: 'Casamigos Reposado Tequila', alcoholic: true, category: ""},
-      { name: 'fresh lemon juice', alcoholic: false, category: ""},
-      { name: 'agave', alcoholic: false, category: "" }
+      { name: 'Angostura Aromatic Bitters', alcoholic: true, category: "bitters"},
+      { name: 'Cointreau Orange Liquer', alcoholic: true, category: "cordial" },
+      { name: 'Casamigos Reposado Tequila', alcoholic: true, category: "tequila"},
+      { name: 'fresh lemon juice', alcoholic: false, category: "other"},
+      { name: 'agave', alcoholic: false, category: "other" }
     ]);
   };
   
