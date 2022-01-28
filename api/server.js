d67736f6d44703366807c1ac1c3c982e67c24b37
Kat <kyevsukov@gmail.com>
2022-01-28 18:32:06 -0500

    added error hadnling middleware

--- api/server.js
+++ api/server.js
@@ -16,4 +16,8 @@ server.get('/', (req, res, next) => {
     res.send('api is up and running!')
 })
 
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ devMessage: err.message, stack: err.stack, prodMessage: 'sorry there was an error'})
+  })
+  
 module.exports = server
