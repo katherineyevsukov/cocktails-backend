6211e0d9040e426c09a3bbf680eb9e5af2a5b65a
Kat <kyevsukov@gmail.com>
2022-01-28 15:36:23 -0500

    set up test files

---
+++ api/tests/coktails.test.js
@@ -0,0 +1,20 @@
+const server = require('./../server')
+const request = require('supertest')
+const db = require('../data/db-config')
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+  })
+  beforeEach(async () => {
+    await db.seed.run()
+  })
+  afterAll(async () => {
+    await db.destroy()
+  })
+
+  describe('sanity', () => {
+      test('test is working', () => {
+          expect(1 + 1).toBe(2)
+      })
+  })
