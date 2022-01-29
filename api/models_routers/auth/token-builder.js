cec38a13495b171e537ae6f34bba3e48fbe0bd01
Kat <kyevsukov@gmail.com>
2022-01-28 19:01:31 -0500

    created token builder file

---
+++ api/models_routers/auth/token-builder.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+const { TOKEN_SECRET } = require("./../config");
+
+module.exports = function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.email,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, TOKEN_SECRET, options);
+};
