28a74045a0b8ea303725367c8ec57294d2da227f
Kat <kyevsukov@gmail.com>
2022-01-29 19:31:22 -0500

    added user find by

---
+++ api/models_routers/auth/auth-middleware.js
@@ -0,0 +1,20 @@
+const Users = require('../users/users-model');
+const bcrypt = require("bcryptjs");
+
+const checkUserValid = async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+      const existingUser = await Users.findBy({ username });
+      if (
+        existingUser &&
+        bcrypt.compareSync(password, existingUser.password)
+      ) {
+        req.userFromDb = existingUser;
+        next();
+      } else {
+        next({ status: 401, message: "invalid credentials" });
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
--- api/models_routers/users/users-model.js
+++ api/models_routers/users/users-model.js
@@ -1,14 +1,35 @@
 const db = require("../../data/db-config");
 
 async function findAll() {
-    const users = await db('users as u')
-      .select('u.id', 'u.email', 'u.first_name', 'u.last_name', 'u.phone')
-      .whereNull('deleted_at')
-      .orderBy('u.id');
-  
-    return users;
-  }
+  const users = await db("users as u")
+    .select("u.id", "u.email", "u.first_name", "u.last_name", "u.phone")
+    .whereNull("deleted_at")
+    .orderBy("u.id");
+
+  return users;
+}
+
+async function findBy(filter) {
+  const user = await db("users as u")
+    .select(
+      "u.id",
+      "u.email",
+      "u.password",
+      "u.first_name",
+      "u.last_name",
+      "u.phone"
+    )
+    .whereNull("deleted_at")
+    .andWhere(filter)
+    .first();
+
+  return user;
+}
+
+findBy({ email: "kat@kat.com" }).then((res) => {
+  console.log(res);
+});
 
 module.exports = {
-    findAll
-}
+  findAll,
+};
