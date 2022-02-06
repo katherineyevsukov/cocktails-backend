8eafabf7d27f2db42af48cb7611b17b0d523b929
Kat <kyevsukov@gmail.com>
2022-02-05 20:46:16 -0500

    feat: added password shape to schema

--- api/models_routers/auth/auth-schemas.js
+++ api/models_routers/auth/auth-schemas.js
@@ -6,6 +6,11 @@ const userSchema = yup.object.shape({
     .typeError("email address must be a string")
     .email("must be a valid email address ")
     .max(255, "email cannot belonger than 255 chars")
-    .required("email is required")
-    
+    .required("email is required"),
+    pasword: yup
+    .string()
+    .typeError("password must be a string")
+    .min(8, "password must be at least 8 chars")
+    .max(30, "password cannot be longer than 30 chars")
+    .required("password is required")
 })
